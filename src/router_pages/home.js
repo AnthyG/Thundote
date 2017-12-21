65995459ee91eb686a989217eb19a413bb1fa194
Anthony Garratt <anthygarratt@aol.de>
2017-12-21 13:04:20 +0100

    Changed some stuff for search; Replaced some text with icons

--- src/router_pages/home.js
+++ src/router_pages/home.js
@@ -41,6 +41,29 @@ var Home = {
             var lists = ["l", "c"];
             var listsn = ["local", "synced"];
             return listsn[lists.indexOf(this.getList)];
+        },
+        p_noteList: function() {
+            var dis = this;
+
+            console.log("Filtering notes", this.hideChecked, this.searchFor);
+
+            if (this.noteList !== null)
+                return this.noteList.filter(function(a, b) {
+                    if (dis.hideChecked && a.todoCheck) {
+                        return false;
+                    }
+
+                    if (dis.searchFor) {
+                        var s = new RegExp(dis.searchFor, "m");
+
+                        if (!s.test(a.body) && !s.test(a.title)) {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                });
+            else return [];
         }
     },
     methods: {
@@ -85,17 +108,21 @@ var Home = {
                     <div class="empty-icon">
                         <i class="mdi">{{ this.getList === 'c' ? 'cloud' : 'cloud_off' }}</i>
                     </div>
-                    <p class="empty-title h5">You have {{ noteList !== null ? noteList.length : 'no' }} {{ curList }} notes</p>
+                    <p class="empty-title h5">
+                        You have {{ p_noteList !== null ? p_noteList.length : 'no' }} {{ curList }} note{{ p_noteList.length === 1 ? '' : 's' }}
+                        <span v-if="searchFor"><i>containing</i> <mark>{{ searchFor }}</mark></span>
+                    </p>
                     <p class="empty-subtitle">Add new notes and tick your completed tasks!</p>
                     <div class="empty-action">
                         <button class="btn btn-primary btn-action" v-on:click.prevent="setGetList(getList, true)"><i class="mdi">refresh</i></button>
-                        <button class="btn btn-primary" v-on:click.prevent="setGetList(true)">Change note-list</button>
-                        <button class="btn btn-primary" v-on:click.prevent="addNote">Add note</button>
+                        <button class="btn btn-primary" v-on:click.prevent="setGetList(true)"><i class="mdi">{{ this.getList === 'c' ? 'cloud' : 'cloud_off' }}</i></button>
+                        <button class="btn btn-primary" v-on:click.prevent="addNote"><i class="mdi">add</i></button>
                         <button class="btn btn-secondary" v-on:click.prevent="toggleHideChecked">{{ hideChecked ? 'Show ticked' : 'Hide ticked' }}</button>
                     </div>
+                    <!--<h5 v-if="searchFor"><br>{{ p_noteList.length }} search-results for <mark>{{ searchFor }}</mark></h5>-->
                 </div>
-                <note-list v-if="noteList !== null"
-                v-bind:noteList="noteList" v-bind:hideChecked="hideChecked" v-bind:searchFor="searchFor"
+                <note-list v-if="p_noteList !== null"
+                v-bind:noteList="p_noteList" v-bind:hideChecked="hideChecked" v-bind:searchFor="searchFor"
                 v-on:editNote="editNote" v-on:todoToggle="todoToggle" v-on:deleteNote="deleteNote"></note-list>
             </div>
             <div v-else>
--- src/vue_components/noteLists.js
+++ src/vue_components/noteLists.js
@@ -30,27 +30,6 @@ Vue.component('note-list', {
     computed: {
         r_noteList: function() {
             return this.noteList.slice(0).reverse();
-        },
-        p_noteList: function() {
-            var dis = this;
-
-            console.log("Filtering notes", this.hideChecked, this.searchFor);
-
-            return this.r_noteList.filter(function(a, b) {
-                if (dis.hideChecked && a.todoCheck) {
-                    return false;
-                }
-
-                if (dis.searchFor) {
-                    var s = new RegExp(dis.searchFor, "m");
-
-                    if (!s.test(a.body) && !s.test(a.title)) {
-                        return false;
-                    }
-                }
-
-                return true;
-            });
         }
     },
     methods: {
@@ -67,7 +46,7 @@ Vue.component('note-list', {
     },
     template: `
         <div v-if="noteList !== null" class="container">
-            <div class="columns" v-for="(note, i) in p_noteList">
+            <div class="columns" v-for="(note, i) in r_noteList">
                 <note-card class="col-12 light" v-bind:index="i" v-bind:key="note.uuid" v-bind:noteData="note"
                 v-on:editNote="editNote" v-on:todoToggle="todoToggle" v-on:deleteNote="deleteNote"></note-card>
             </div>
